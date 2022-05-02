851dc00126b2de918440a1e9f92a3aceb82d61d1
Valentin Voica <valentin_voiclla@live.com>
2022-05-02 18:05:17 +0300

    Add project files.

---
+++ won4Lab2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace won4Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
